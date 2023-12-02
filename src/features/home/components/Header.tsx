b5224da0075a8ea811c1708081f241805436bce3
tatenodev <gd28.c.tateno@gmail.com>
2023-12-02 15:05:41 +0900

    add dialog

--- src/features/home/components/Header.tsx
+++ src/features/home/components/Header.tsx
@@ -1,74 +1,15 @@
-"use client";
+import { Flex, Heading } from "@radix-ui/themes";
 
-import { useState } from "react";
-import { Button, Flex, Heading, Dialog, Text, TextField } from "@radix-ui/themes";
-
-import { shareArticle } from "../serverActions/shareArticle";
+import { HeaderPostDialog } from "./HeaderPostDialog";
 
 export function Header() {
-  const [shareUrl, setShareUrl] = useState("");
-  const [shareComment, setShareComment] = useState("");
-
-  const handleShare = async () => {
-    const res = await shareArticle({ url: shareUrl, comment: shareComment });
-    console.log(`res: ${res}`);
-    setShareUrl("");
-    setShareComment("");
-  };
-
   return (
     <header style={{ display: "flex", justifyContent: "center" }}>
       <Flex style={{ maxWidth: "800px", width: "100%" }} justify="between">
         <Heading mb="5" color="blue" size="6">
           siciliano
         </Heading>
-
-        <Dialog.Root>
-          <Dialog.Trigger>
-            <Button>Add</Button>
-          </Dialog.Trigger>
-
-          <Dialog.Content style={{ maxWidth: 450 }}>
-            <Dialog.Title>Share</Dialog.Title>
-            <Dialog.Description size="2" mb="4">
-              シェアしたい記事のURLを入力してコメントを添えよう
-            </Dialog.Description>
-
-            <Flex direction="column" gap="3">
-              <label>
-                <Text as="div" size="2" mb="1" weight="bold">
-                  URL
-                </Text>
-                <TextField.Input
-                  defaultValue={shareUrl}
-                  onChange={(e) => setShareUrl(e.target.value)}
-                  placeholder="https://"
-                />
-              </label>
-              <label>
-                <Text as="div" size="2" mb="1" weight="bold">
-                  Comment
-                </Text>
-                <TextField.Input
-                  defaultValue={shareComment}
-                  onChange={(e) => setShareComment(e.target.value)}
-                  placeholder="記事についてのコメントを書こう"
-                />
-              </label>
-            </Flex>
-
-            <Flex gap="3" mt="4" justify="end">
-              <Dialog.Close>
-                <Button variant="soft" color="gray">
-                  Cancel
-                </Button>
-              </Dialog.Close>
-              <Dialog.Close>
-                <Button onClick={handleShare}>Post</Button>
-              </Dialog.Close>
-            </Flex>
-          </Dialog.Content>
-        </Dialog.Root>
+        <HeaderPostDialog />
       </Flex>
     </header>
   );
---
+++ src/features/home/components/HeaderPostDialog.tsx
@@ -0,0 +1,131 @@
+"use client";
+
+import { Dispatch, SetStateAction, useState } from "react";
+import { Button, Flex, Dialog, Text, TextField } from "@radix-ui/themes";
+
+import { shareArticle } from "../serverActions/shareArticle";
+
+type User = {
+  name: string;
+};
+
+export function HeaderPostDialog() {
+  const [localUser, setLocalUser] = useState<User | null>(null);
+  const [inputName, setInputName] = useState("");
+
+  const handleUserName = () => {
+    const userStringify = window.localStorage.getItem("user");
+    const user: User | null = userStringify ? JSON.parse(userStringify) : null;
+    setLocalUser(user);
+  };
+
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger>
+        <Button onClick={handleUserName}>Add</Button>
+      </Dialog.Trigger>
+      <Dialog.Content style={{ maxWidth: 450 }}>
+        {localUser?.name ? (
+          <ArticleInputContent />
+        ) : (
+          <UserNameInputContent inputName={inputName} setInputName={setInputName} setLocalUser={setLocalUser} />
+        )}
+      </Dialog.Content>
+    </Dialog.Root>
+  );
+}
+
+type UserNameInputContentProps = {
+  inputName: string;
+  setInputName: Dispatch<SetStateAction<string>>;
+  setLocalUser: Dispatch<SetStateAction<User>>;
+};
+
+const UserNameInputContent = ({ inputName, setInputName, setLocalUser }: UserNameInputContentProps) => {
+  const handleSetName = () => {
+    if (inputName === "") return alert("名前を入力してください");
+    window.localStorage.setItem("user", JSON.stringify({ name: inputName }));
+    setLocalUser({ name: inputName });
+  };
+
+  return (
+    <>
+      <Dialog.Title>ニックネーム</Dialog.Title>
+      <Dialog.Description size="2" mb="4">
+        あなたの名前を入力してください
+      </Dialog.Description>
+
+      <Flex direction="column" gap="3">
+        <label>
+          <TextField.Input
+            defaultValue={inputName}
+            onChange={(e) => setInputName(e.target.value)}
+            placeholder="Faure"
+          />
+        </label>
+      </Flex>
+
+      <Flex gap="3" mt="4" justify="end">
+        <Dialog.Close>
+          <Button variant="soft" color="gray">
+            キャンセル
+          </Button>
+        </Dialog.Close>
+        <Button onClick={handleSetName}>保存</Button>
+      </Flex>
+    </>
+  );
+};
+
+const ArticleInputContent = () => {
+  const [shareUrl, setShareUrl] = useState("");
+  const [shareComment, setShareComment] = useState("");
+
+  const handleShare = async () => {
+    const res = await shareArticle({ url: shareUrl, comment: shareComment });
+    setShareUrl("");
+    setShareComment("");
+  };
+  return (
+    <>
+      <Dialog.Title>Share</Dialog.Title>
+      <Dialog.Description size="2" mb="4">
+        シェアしたい記事のURLを入力してコメントを添えよう
+      </Dialog.Description>
+
+      <Flex direction="column" gap="3">
+        <label>
+          <Text as="div" size="2" mb="1" weight="bold">
+            URL
+          </Text>
+          <TextField.Input
+            defaultValue={shareUrl}
+            onChange={(e) => setShareUrl(e.target.value)}
+            placeholder="https://"
+          />
+        </label>
+        <label>
+          <Text as="div" size="2" mb="1" weight="bold">
+            Comment
+          </Text>
+          <TextField.Input
+            defaultValue={shareComment}
+            onChange={(e) => setShareComment(e.target.value)}
+            placeholder="記事についてのコメントを書こう"
+          />
+        </label>
+      </Flex>
+
+      <Flex gap="3" mt="4" justify="end">
+        <Dialog.Close>
+          <Button variant="soft" color="gray">
+            キャンセル
+          </Button>
+        </Dialog.Close>
+        <Dialog.Close>
+          <Button onClick={handleShare}>投稿</Button>
+        </Dialog.Close>
+      </Flex>
+    </>
+  );
+};
